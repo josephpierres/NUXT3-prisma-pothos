165c18607fecc74a14ae3d62bda1b6230c96a6f1
josephpierres <josephpierres@hotmail.com>
2022-06-22 22:13:24 -0400

    mercredi 22 juin 2022: ajout de pothos prisma plugin

--- server/schema.ts
+++ server/schema.ts
@@ -2,19 +2,23 @@ import { builder } from './builder';
 import { db } from '../prisma/db';
 
 builder.prismaNode("User", {
-  findUnique: (id) => ({ id }),
-  id: { resolve: (user) => user.id },
-    fields: (t) => ({
+  // This is used to load the node by id
+  findUnique: (id) => ({ id }), // the findUnique function now only receives an id. This is to support relays ability to load nodes by id.
+  // This is used to get the id from a node 
+  id: { resolve: (user) => user.id }, //there is a new id option that mirrors the id option from node method of the relay plugin, and must contain a resolve function that returns the id from an instance of the node. 
+    
+  fields: (t) => ({
       name: t.exposeString('name', { nullable: true, }),
       email: t.exposeString('email', { nullable: true, }),
       emailVerified: t.string({ 
-        resolve: (parent) => parent.emailVerified?.toDateString(),
-        nullable: true, 
-      }),
+            resolve: (parent) => parent.emailVerified?.toDateString(),
+            nullable: true, 
+            }),
       image: t.exposeString('image', { nullable: true, }),
       role: t.exposeString('role'),
       // Load posts as list field.
-      posts: t.relation('posts', {
+      postsConnection: t.relatedConnection('posts', {
+        cursor: 'id',
         args: {
           oldestFirst: t.arg.boolean(),
         },
@@ -26,9 +30,7 @@ builder.prismaNode("User", {
         },
       }),
     }),
-    postsConnection: t.relatedConnection('posts', {
-      cursor: 'id',
-    }),
+    
   }),
 });
 
@@ -57,8 +59,9 @@ builder.queryType({
           //where: { id: Number.parseInt(String(args.id), 10) },
         }),
     }),
-    posts: t.prismaField({
-      type: ['Post'],
+    postsConnection: t.prismaConnection({
+      type: 'Post',
+      cursor: 'id',
       args: {
         take: t.arg.int(),
         skip: t.arg.int(),
